e95b3dd880be35f5378419b20ba102e042349501
gracemcken <grace.ellen.mckenna@gmail.com>
2023-03-08 23:49:29 +0000

    Add basic navbar and urls for each main page

--- glam_dog/urls.py
+++ glam_dog/urls.py
@@ -20,5 +20,9 @@ from glamdog_booking import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('services/', views.services_view, name='services'),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
     path('admin/', admin.site.urls),
 ]
--- glamdog_booking/views.py
+++ glamdog_booking/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, "pages/home.html", {})
+
+
+def register_view(request):
+    return render(request, "pages/register.html", {})
+
+
+def about_view(request):
+    return render(request, "pages/about.html", {})
+
+
+def contact_view(request):
+    return render(request, "pages/contact.html", {})
+
+
+def services_view(request):
+    return render(request, "pages/services.html", {})
